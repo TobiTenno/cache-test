9d40762c2c18a6fa0530dce1075c0bdec6891b7b
Matej Voboril <tobiah@pm.me>
2024-03-01 16:56:55 -0600

    add ora to demonstrate what happens

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ import flatCache from 'flat-cache';
 import data from 'warframe-worldstate-data';
 import {dirname, resolve} from 'node:path';
 import {fileURLToPath} from "node:url";
+import ora from 'ora';
 
 const dirName = dirname(fileURLToPath(import.meta.url));
 const i18nOnObject = true;
 const caches = ['weapons', 'warframes', 'items', 'mods'];
+let spinner = ora('Caching items').start();
 
 const cache = flatCache.load('.items', resolve(dirName));
 
+process.on('unhandledRejection', (reason, p) => {
+  spinner = spinner.fail(`Unhandled Rejection at: Promise ${p} reason: ${reason}`);
+});
+process.on("uncaughtException", (err) => {
+  spinner = spinner.fail(`Uncaught Exception thrown: ${err.message}`);
+});
+
 const makeLanguageCache = (language) => {
+  spinner = spinner.start(`Constructing cache for ${language}`);
   const base = {
     weapons: new Items({
       category: ['Primary', 'Secondary', 'Melee', 'Arch-Melee', 'Arch-Gun'],
@@ -69,12 +79,15 @@ data.locales.forEach((language) => {
   });
 });
 try {
+  spinner = spinner.start('Setting last_updt');
   cache.setKey('last_updt', Date.now());
 } catch (e) {
   console.error('failed to set last_updt', e);
 }
 try {
+  spinner = spinner.start('Saving cache');
   cache.save(true);
 } catch (e) {
   console.error('failed to save cache', e);
 }
+spinner = spinner.succeed('Saved');
